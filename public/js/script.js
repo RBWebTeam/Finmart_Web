e72ff7343fc14574e9d92c652eb2e6a8ed7a7fcb
manish <manish@Manishdixit>
2018-02-07 14:45:19 +0530

    web app

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
-  const fileInput = document.getElementById('file-input');
-
-  fileInput.addEventListener('change', (e) => console.log(e.target.files));
+const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', (e) => console.log(e.target.files));
+navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+        .then(function (registration)
+        {
+          console.log('Service worker registered successfully');
+        }).catch(function (e)
+        {
+          console.error('Error during service worker registration:', e);
+        });
